cbdeed5407629d1e04d4d28a68f3df5d4c3eeebe
Berman1990 <berman1990@gmail.com>
2015-06-11 13:06:44 -0700

    parameters file read parameters file function to main

--- HelloRobot/main.cpp
+++ HelloRobot/main.cpp
@@ -87,6 +87,85 @@ void NewBlow(int xPos,int yPos,int nCount)
 
 }
 
+void ReadParametersFile()
+{
+	struct StartLocationType {
+	  string Xpos;
+	  string Ypos;
+	  string Yaw;;
+	} ;
+	struct GoalLocationType {
+	  string Xpos;
+	  string Ypos;
+	} ;
+
+	string temp1, temp2, temp3;
+	StartLocationType StartLocation;
+	GoalLocationType Goal;
+	string FileContent, FileVariable, mapLocation;
+	int MapResolutionCM, GridResolutionCM , RobotSize;
+	ifstream infile;
+	size_t Pos;
+
+	infile.open ("parameters.txt");
+	if (infile.is_open()) {
+	while(!infile.eof())
+        {
+			getline(infile,FileContent);
+			Pos = FileContent.find(": ");
+			FileVariable = FileContent.substr(0,Pos);
+			if (!FileVariable.compare("map"))
+			{
+				mapLocation = FileContent.substr(Pos+2,FileContent.length());
+				cout << mapLocation << endl;
+			}
+			else if (!FileVariable.compare("startLocation"))
+			{
+				FileContent = FileContent.substr(Pos+2,FileContent.length());
+
+				Pos = FileContent.find(" ");
+				StartLocation.Xpos = FileContent.substr(0,Pos);
+				FileContent = FileContent.substr(Pos+1,FileContent.length());
+
+				Pos = FileContent.find(" ");
+				StartLocation.Ypos = FileContent.substr(0,Pos);
+				FileContent = FileContent.substr(Pos+1,FileContent.length());
+
+				StartLocation.Yaw = FileContent.substr(0,Pos-1);
+			}
+			else if (!FileVariable.compare("goal"))
+			{
+				FileContent = FileContent.substr(Pos+2,FileContent.length());
+
+				Pos = FileContent.find(" ");
+				Goal.Xpos = FileContent.substr(0,Pos);
+				FileContent = FileContent.substr(Pos+1,FileContent.length());
+
+				Goal.Ypos = FileContent.substr(0,Pos);
+			}
+			else if (!FileVariable.compare("robotSize"))
+			{
+				FileContent = FileContent.substr(Pos+2,FileContent.length());
+				Pos = FileContent.find(" ");
+				temp1 = FileContent.substr(0,Pos);
+
+				//RobotSize  = std::stoi(temp1.c_str());
+			}
+			else if (!FileVariable.compare("MapResolutionCM"))
+			{
+				temp2 = FileContent.substr(Pos+2,FileContent.length()-1);
+				//MapResolutionCM  = atoi(temp2.c_str());
+			}
+			else if (!FileVariable.compare("GridResolutionCM"))
+			{
+				temp3 = FileContent.substr(Pos+2,FileContent.length()-1);
+				//GridResolutionCM  = atoi(temp3.c_str());
+			}
+        }
+	}
+	infile.close();
+
+}
 
 
 int main() {
