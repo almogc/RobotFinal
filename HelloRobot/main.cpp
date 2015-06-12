06b97bf81ad0d60210dbd01d6722bc9ccb771913
Berman1990 <berman1990@gmail.com>
2015-06-11 23:59:20 -0700

    ReadParamFileFix

--- HelloRobot/main.cpp
+++ HelloRobot/main.cpp
@@ -91,20 +91,19 @@ void NewBlow(int xPos,int yPos,int nCount)
 void ReadParametersFile()
 {
 	struct StartLocationType {
-	  string Xpos;
-	  string Ypos;
-	  string Yaw;;
+	  int Xpos;
+	  int Ypos;
+	  int Yaw;;
 	} ;
 	struct GoalLocationType {
-	  string Xpos;
-	  string Ypos;
+	  int Xpos;
+	  int Ypos;
 	} ;
 
-	string temp1, temp2, temp3;
 	StartLocationType StartLocation;
 	GoalLocationType Goal;
 	string FileContent, FileVariable, mapLocation;
-	int MapResolutionCM, GridResolutionCM , RobotSize;
+	double MapResolutionCM, GridResolutionCM , RobotSize;
 	ifstream infile;
 	size_t Pos;
 
@@ -125,42 +124,39 @@ void ReadParametersFile()
 				FileContent = FileContent.substr(Pos+2,FileContent.length());
 
 				Pos = FileContent.find(" ");
-				StartLocation.Xpos = FileContent.substr(0,Pos);
+				StartLocation.Xpos = atoi(FileContent.substr(0,Pos).c_str());
 				FileContent = FileContent.substr(Pos+1,FileContent.length());
 
 				Pos = FileContent.find(" ");
-				StartLocation.Ypos = FileContent.substr(0,Pos);
+				StartLocation.Ypos = atoi(FileContent.substr(0,Pos).c_str());
 				FileContent = FileContent.substr(Pos+1,FileContent.length());
 
-				StartLocation.Yaw = FileContent.substr(0,Pos-1);
+				StartLocation.Yaw = atoi(FileContent.substr(0,Pos-1).c_str());
 			}
 			else if (!FileVariable.compare("goal"))
 			{
 				FileContent = FileContent.substr(Pos+2,FileContent.length());
 
 				Pos = FileContent.find(" ");
-				Goal.Xpos = FileContent.substr(0,Pos);
+				Goal.Xpos = atoi(FileContent.substr(0,Pos).c_str());
 				FileContent = FileContent.substr(Pos+1,FileContent.length());
 
-				Goal.Ypos = FileContent.substr(0,Pos);
+				Goal.Ypos = atoi(FileContent.substr(0,Pos).c_str());
 			}
 			else if (!FileVariable.compare("robotSize"))
 			{
 				FileContent = FileContent.substr(Pos+2,FileContent.length());
 				Pos = FileContent.find(" ");
-				temp1 = FileContent.substr(0,Pos);
 
-				RobotSize = atoi(temp1.c_str());
+				RobotSize = atof(FileContent.substr(0,Pos).c_str());
 			}
 			else if (!FileVariable.compare("MapResolutionCM"))
 			{
-				temp2 = FileContent.substr(Pos+2,FileContent.length()-1);
-				MapResolutionCM  = atoi(temp2.c_str());
+				MapResolutionCM  = atof(FileContent.substr(Pos+2,FileContent.length()-1).c_str());
 			}
 			else if (!FileVariable.compare("GridResolutionCM"))
 			{
-				temp3 = FileContent.substr(Pos+2,FileContent.length()-1);
-				GridResolutionCM  = atoi(temp3.c_str());
+				GridResolutionCM  = atof(FileContent.substr(Pos+2,FileContent.length()-1).c_str());
 			}
         }
 	}
@@ -171,6 +167,7 @@ void ReadParametersFile()
 
 int main() {
 
+	ReadParametersFile();
 	const char* filename = "Maps/roboticLabMap.png";
 		loadImage(filename);
 
